9dd085253d3ff82609150422dd34e2c62cc39967
Peacemaker228 <alanchik99@gmail.com>
2023-12-05 17:54:54 +0300

    merge main into task-4

---
+++ src/pages/HomeWork3/_mocks.ts
@@ -0,0 +1,32 @@
+import { RowType } from "./types";
+
+export const tableData: RowType[] = [
+  {
+    id: 15,
+    userId: 1,
+    title: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+  },
+  {
+    id: 42,
+    userId: 2,
+    title: "Nulla, quo libero! Ipsa fugiat nam veniam",
+  },
+  {
+    id: 168,
+    userId: 15,
+    title:
+      "itaque, praesentium dolorem iure magni repudiandae ut illo doloremque corrupti.",
+  },
+  {
+    id: 98,
+    userId: 3,
+    title: "Voluptatem iusto veritatis eius delectus.",
+  },
+  {
+    id: 1337,
+    userId: 4,
+    title: "Hello World!",
+  },
+];
+
+export const tableHeaderData = ["userId", "id", "title"];
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,8 @@
 import { ReactElement } from "react";
 import { HomeWork1 } from "./pages/HomeWork1/HomeWork1";
-import { HomeWork2 } from "./pages/HomeWork2/HomeWork2";
 import { HomeWork1Second } from "./pages/HomeWork1_second/HomeWork1Second";
+import { HomeWork2 } from "./pages/HomeWork2/HomeWork2";
+import { HomeWork3 } from "./pages/HomeWork3/HomeWork3";
 import { HomeWork4 } from "./pages/HomeWork4/HomeWork4";
 
 interface IRoute {
@@ -11,19 +12,23 @@ interface IRoute {
 }
 
 export const routesConfig: Record<string, IRoute> = {
-  ["/homework1"]: {
+  "/homework1": {
     title: "Домашнее задание №1",
     element: <HomeWork1 />,
   },
-  ["/homework1-second"]: {
+  "/homework1-second": {
     title: "Домашнее задание №1 (second)",
     element: <HomeWork1Second />,
   },
-  ["/homework2"]: {
+  "/homework2": {
     title: "Домашнее задание №2",
     element: <HomeWork2 />,
   },
-  ["/homework4"]: {
+  "/homework3": {
+    title: "Домашнее задание №3",
+    element: <HomeWork3 />,
+  },
+  "/homework4": {
     title: "Домашнее задание №4",
     element: <HomeWork4 />,
   },
